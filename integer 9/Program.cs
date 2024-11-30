cec0aa6fb97a2fd50e3da98e7b13cdc39ed74edd
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-30 23:06:29 +0500

    js

---
+++ integer 9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace integer_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = a / 100;
+            Console.WriteLine(a);
+        }
+    }
+}
